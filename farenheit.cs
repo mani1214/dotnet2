1509965aa10451e1e503eac40f2c059dd1e3a4b4
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-08 14:11:13 +0530

    Add files via upload

---
+++ farenheit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fahrenheittocelsuis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter fahrenheit:");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine((fahrenheit - 32)*5 / 9);
+            Console.ReadLine();
+        }
+    }
+}
