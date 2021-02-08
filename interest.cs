40756d3b95a1203f56a8980d3054cd429cf1460f
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-08 14:11:40 +0530

    Add files via upload

---
+++ interest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleinterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 10000;
+            double interst = 5 ;
+            int time = 12;
+            Console.WriteLine((amount * interst * time) / 100);
+            Console.ReadLine();
+        }
+    }
+}
