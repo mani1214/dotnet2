203f6fd22eed09b12f90a579bc9df5ec759275b2
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-08 14:07:26 +0530

    Add files via upload

---
+++ arrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { 3, 4 }, { 6, 9 } };
+            int[,] arr1 = { { 1, 7 }, { 2, 5 } };
+            for(int i=0;i<2;i++)
+            {
+                for(int j=0;j<2;j++)
+                {
+                    Console.Write(arr[i,j]+arr1[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
