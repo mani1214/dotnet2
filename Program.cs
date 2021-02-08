3dba1594f7977ddd61d0fbf0fcb4f272a0dded8d
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-08 14:07:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dollarstorupees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number of dollars:");
+            double dollars = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(dollars * 72.92);
+            Console.ReadLine();
+        }
+    }
+}
